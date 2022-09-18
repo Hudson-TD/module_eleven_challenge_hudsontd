3abc40534e59a912d6723a408d0cdda9a10087d0
Tyler Hudson <tylerhudson96@gmail.com>
2022-09-18 12:11:32 -0400

    Basic routes setup and communicating with HTML

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path'); 
+
+const PORT = process.env.PORT || 3001; 
+const app = express(); 
+
+app.use(express.urlencoded ( { extended: true }));
+app.use(express.json());
+app.use(express.static('public')); 
+
+const { notes } = require('./Develop/db/db.json');
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'./Develop/public/index.html'));
+}); 
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname,'./Develop/public/notes.html'));
+}); 
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
