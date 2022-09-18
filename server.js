45c71f44e4bdbc3a75e1367d051f833bd08e3f23
Tyler Hudson <tylerhudson96@gmail.com>
2022-09-18 16:37:52 -0400

    Fixed static configuration causing issues with style sheet loading

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
+// Global imports
 const express = require('express');
 const fs = require('fs');
 const path = require('path'); 
 
-const PORT = process.env.PORT || 3001; 
-const app = express(); 
+//Port structure for hosting and local machine
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const noteSource = require('./db/db.json');
 
 app.use(express.urlencoded ( { extended: true }));
 app.use(express.json());
 app.use(express.static('public')); 
 
-const { notes } = require('./Develop/db/db.json');
-
-
+// Setting default routing to our index.html file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'./Develop/public/index.html'));
-}); 
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
+app.get('/api/notes', (req, res) => {
+  res.json(noteSource);
+});
+
+// Get routing for our notes.html file
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname,'./Develop/public/notes.html'));
-}); 
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
+// Setting up port
 app.listen(PORT, () =>
+// General console log for easy http link access
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
